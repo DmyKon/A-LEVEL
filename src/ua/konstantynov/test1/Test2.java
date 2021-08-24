aadaa3e57288535dd7d56ae5cde5864603f442b0
Dmytro Konstantynov <qdimap1@gmail.com>
2021-08-24 11:48:57 +0300

    test1

--- src/ua/konstantynov/test1/Test2.java
+++ src/ua/konstantynov/test1/Test2.java
@@ -11,15 +11,15 @@ public class Test2 {
     }
 
     public static boolean knightsMove(String current, String target) {
-        int curNum = Integer.parseInt(current.substring(1, 2));
+        int curNum = current.charAt(1) - 0x30;
+        int tarNum = target.charAt(1) - 0x30;
         int curLet = current.toLowerCase().charAt(0) - 0x60;
-        int tarNum = Integer.parseInt(target.substring(1, 2));
         int tarLet = target.toLowerCase().charAt(0) - 0x60;
         int difNum = Math.abs(curNum - tarNum);
         int difLet = Math.abs(curLet - tarLet);
         return (difNum == 1 && difLet == 2 || difNum == 2 && difLet == 1) &&
-                (tarNum >= 1 && tarNum <= 8 && tarLet >= 1 && tarLet <= 8) &&
-                (curNum >= 1 && curNum <= 8 && curLet >= 1 && curLet <= 8);
+                tarNum >= 1 && curNum >= 1 && tarLet >= 1 && curLet >= 1 &&
+                tarNum <= 8 && curNum <= 8 && tarLet <= 8 && curLet <= 8;
     }
 
 }
