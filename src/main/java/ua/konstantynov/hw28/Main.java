24c740f22ce6bba20cd0bd2d21ac6093d063d185
Dmytro Konstantynov <qdimap1@gmail.com>
2021-11-06 22:55:29 +0200

    hw27

---
+++ src/main/java/ua/konstantynov/hw28/Main.java
@@ -0,0 +1,14 @@
+//This home task contains 3 items:
+//1. Create cloud Linux machine, upload your previous home task which was about creation bash-script and
+// ensure that your functionality is working properly on Heroku
+//2. Create a java application which is able to calculate the main trigonometric functions,
+// deploy your application to Heroku and ensure that the application is working properly.
+//3. Create a podcast which describes the main principles of working with Heroku.
+
+package ua.konstantynov.hw28;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Heroku");
+    }
+}
